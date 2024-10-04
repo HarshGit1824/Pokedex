33186f26e0b1ab24866efdb408eae276b4df28ba
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-10-04 08:41:02 +0000

    focus input slash

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@
 // First target the input
 const inputE1 = document.querySelector('input');
 // Log the input element
-document.addEventListener('keypress', function () {
-  console.log('waah');
+document.addEventListener('keypress', function (event) {
+  if (event.key === '/') {
+    event.preventDefault();
+    inputE1.focus();
+  }
 });
