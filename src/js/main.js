d5106014f2851de6983b85506251d3c58b2c9500
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-10-04 09:29:55 +0000

    render pokemonn cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,39 @@
+import data from './data.json';
+
 // === Dom Selection ===
 // First target the input
-const inputE1 = document.querySelector('input');
-// Log the input element
+const inputEl = document.querySelector('input');
+const rowE1 = document.querySelector('#row');
+
+// show pokemom name in paragraph inside the row
+for (let pokemonObj of data) {
+  const div = document.createElement('div');
+  div.classList.add('col');
+
+  div.innerHTML = `
+  <div class="card">
+            <img
+              src="${pokemonObj.image}"
+              class="card-img-top"
+              alt="${pokemonObj.name}"
+            />
+            <div class="card-body">
+              <h5 class="card-title">${pokemonObj.name}</h5>
+              <p class="card-text">
+              ${pokemonObj.description}
+              </p>
+            </div>
+          </div>`;
+
+  rowE1.appendChild(div);
+}
+
+function renderPokemons(list) {}
+
+// add to focus input
 document.addEventListener('keypress', function (event) {
   if (event.key === '/') {
-    event.preventDefault();
-    inputE1.focus();
+    event.preventDefault(); // esko (/) likhna nhi he
+    inputEl.focus();
   }
 });
