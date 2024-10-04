13872763ab4901852e109c5bd97f4cc8b8433fbd
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-10-04 08:31:52 +0000

    add data.json

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// === Dom Selection ===
+// First target the input
+const inputE1 = document.querySelector('input');
+// Log the input element
+document.addEventListener('keypress', function () {
+  console.log('waah');
+});
